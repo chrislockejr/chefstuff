366eb5be0270da97ccf472c7199ce798d44bd31e
Chris Locke <chris.locke.jr@gmail.com>
2019-01-03 22:15:52 +0000

    working on web recipe and ubuntu test issues

--- cookbooks/web/recipes/default.rb
+++ cookbooks/web/recipes/default.rb
@@ -3,4 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-package "nginx"
+# Install the apache package
+
+if node['platform_family'] == "rhel"
+  package = "httpd"
+elsif node['platform_family'] == "debian"
+  package = "apache2"
+end
+
+package 'apache2' do
+  package_name package
+  action :install
+end
+
+service 'apache2' do
+  service_name package
+  action [:start, :enable]
+end
